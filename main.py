211cca900cfa6451387c066763176f7d2359bce0
bth123 <133943325+bth123@users.noreply.github.com>
2023-12-10 18:12:27 +0200

    forgor to upload latest

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import database
 import re
 from json import loads
 
@@ -10,18 +9,21 @@ class Highlighter:
 		color_codes = database_content["color_codes"]
 		commands = database_content["commands"]
 		regexes = {
-			"nbt": r'(\$\([a-zA-Z0-9_-]*\)|"[^"]*"|\'[^\']*\'|[a-zA-Z_]\w*|[0-9]+[a-zA-Z]?|[\[\]{},:;])',
+			"nbt": r"""(\$\([a-zA-Z0-9_-]*\)|string[0-9]+|[a-zA-Z_]\w*|[0-9]+[a-zA-Z]?|[\[\]{},:;])""",
+			"string": r"""(?<!\\)(?:"(?:\\.|[^"])*"|\'(?:\\.|[^\'])*\')""",
 			"macro": r"\$\([a-zA-Z0-9_-]*\)"
 		}
+		root_command = ""
 
 	def highlight(function):
 		highlighted = ""
+		function = function.replace("\\\n", "\\newline")
 		function = function.split("\n")
 		for command in function:
-			highlighted += f"{Highlighter.command(command)}\n"
+			highlighted += f"{Highlighter.line(command)}\n"
 		return highlighted
 
-	def command(command):
+	def line(command):
 		# Shortcuts
 		colors = Highlighter.Database.color_codes
 		commands = Highlighter.Database.commands
@@ -32,7 +34,7 @@ class Highlighter:
 		if raw_command == "":
 			return raw_command
 		elif raw_command[0] == "#":
-			if raw_command[1] == ">":
+			if raw_command[1] in "#>":
 				return " "*tabs_count + colors["comment"] + "#>" + colors["link-comment"] + raw_command[2:]
 			else:
 				return f"{' '*tabs_count}{colors['comment']}{raw_command}"
@@ -40,10 +42,10 @@ class Highlighter:
 		command = Highlighter.split(raw_command)
 		highlighted = []
 		for element in command:
-			if (raw_root := element.replace("$", "")) in commands:
+			if (raw_root := element.replace("$", "").replace("\\newline", "")) in commands:
 				highlighted.append(colors["command"]+element)
 				Highlighter.root_command = raw_root
-			elif element in commands[Highlighter.root_command]["subcommands"]:
+			elif Highlighter.root_command != "" and element.replace("\\", "") in commands[Highlighter.root_command]["subcommands"]:
 				highlighted.append(colors["subcommand"]+element)
 			elif element[0] == "@":
 				highlighted.append(Highlighter.target(element))
@@ -55,32 +57,27 @@ class Highlighter:
 				highlighted.append(colors["path"]+element)
 			elif element.isdigit():
 				highlighted.append(colors["number"]+element)
+			elif element[:-2] == "$(":
+				highlighted.append(colors["macro"]+"$"+colors["bracket0"]+"["+colors["argument"]+element[2:-1]+colors["bracket0"]+"]")
 			else:
 				highlighted.append(colors["argument"]+element)
-			if element[:-2] == "$(":
-				highlighted.append(colors["macro"]+"$"+colors["bracket0"]+"["+colors["argument"]+element[2:-1]+colors["bracket0"]+"]")
-		return f"{' ' * tabs_count}{' '.join(highlighted)}"
-
-	def macro(element, prev_color):
-		colors = Highlighter.Database.color_codes
-		highlighted = ""
-		for index, symbol in enumerate(element):
-			if symbol == "$":
-				highlighted += colors["macro"] + symbol
-			elif symbol in "()":
-				highlighted += colors[f"bracket{brackets_depth}"] + symbol
-				if symbol == ")" and element[idnex+1:index+2] not in "$":
-					highlighted += colors[prev_color]
-			else:
-				highlighted += symbol
+		return f"{' ' * tabs_count}{' '.join(highlighted)}".replace("newline", f"{colors['backslash']}\\\n")
 
 	def split(command):
 		command += " "
 		command_elements = []
+		strings = []
 		brackets_count = 0
 		current_element = ""
-		for char in command:
-			if char in "{[":
+		for index, char in enumerate(command):
+			if len(strings) >= 1:
+				if char == strings[-1] and command[index-1] != "\\":
+					strings.pop(-1)
+				current_element += char
+			elif char in "\"'":
+				strings.append(char)
+				current_element += char
+			elif char in "{[":
 				brackets_count += 1
 				current_element += char
 			elif char in "]}":
@@ -116,11 +113,11 @@ class Highlighter:
 						curr_tag = ""
 				elif brackets_count >= 1:
 					curr_tag += char
-		highlighted = colors["selector"]+selector.replace("[", f"{colors['bracket1']}[{colors['key']}").replace("]", f"{colors['bracket1']}]")\
+		highlighted = colors["selector"]+selector.replace("[", f"{colors['bracket0']}[{colors['key']}").replace("]", f"{colors['bracket0']}]")\
 			.replace("=", f"{colors['delimiter']}={colors['value']}").replace(",", f"{colors['delimiter']},{colors['key']}")\
-				.replace("!", f"{colors['bracket2']}!{colors['value']}").replace("..", f"{colors['bracket2']}..{colors['value']}")\
-					.replace("#", f"{colors['selector']}#").replace("{", colors['bracket2']+"{"+colors['key'])\
-					.replace("}", colors['bracket2']+"}"+colors['value'])
+				.replace("!", f"{colors['delimiter']}!{colors['value']}").replace("..", f"{colors['bracket1']}..{colors['value']}")\
+					.replace("#", f"{colors['selector']}#").replace("{", colors['bracket1']+"{"+colors['key'])\
+					.replace("}", colors['bracket1']+"}"+colors['value'])
 		if "nbts" in locals():
 			for index, tag in enumerate(nbts):
 				highlighted = highlighted.replace(f"%%%%nbt{index}%%%%", Highlighter.nbt(tag, brackets_depth=0))
@@ -128,15 +125,21 @@ class Highlighter:
 
 	def nbt(tag, brackets_depth=-1):
 		colors = Highlighter.Database.color_codes
+		# Strings are highlighted separately bc they has really hard regex and idk
+		# if there a way to put it into nbt regex
+		string_re = Highlighter.Database.regexes["string"]
+		strings = re.findall(string_re, tag)
+		for index, string in enumerate(strings):
+			tag = tag.replace(string, f"string{index}")
 		def lexer(tag, brackets_depth):
 			tokens = []
-			regex = Highlighter.Database.regexes["nbt"]
-			for match in re.finditer(regex, tag):
-				token = match.group(1)
-				if token.lstrip()[0] in "\"'":
-					tokens.append((token, "str_value"))
-				elif token[0] in "1234567890" or token in "BIL":
+			nbt_re = Highlighter.Database.regexes["nbt"]
+			for match in re.finditer(nbt_re, tag):
+				token = match.group()
+				if token[0] in "1234567890" or token in "BIL":
 					tokens.append((token, "value"))
+				elif token[:6] == "string":
+					tokens.append((token, "string"))
 				elif token in "[{":
 					brackets_depth += 1
 					brackets_depth %= 3
@@ -155,17 +158,13 @@ class Highlighter:
 		lexed = lexer(tag, brackets_depth)
 		highlighted = ""
 		for token, type in lexed:
-			if token[0] == "$":
-				highlighted += colors["macro"]+"$"+colors[type]+"("+colors["argument"]+token[2:-1]+colors[type]+")"
+			if token[:2] == "$(":
+				highlighted += f"{colors['macro']}${colors[type]}({colors['argument']}{token[2:-1]}{colors[type]})"
+			elif type == "string":
+				highlighted += f"{colors['str_value']}{strings[int(token.replace('string', ''))]}"
 			else:
 				highlighted += colors[type] + token
 				if type == "delimiter":
 					highlighted += " "
 		return highlighted
 
-a = "$data $(macro_yeaah) entity @s[type=armor_stand] {Invisible: 1b, ArmorItems:[{},{},{},{id:\"emerald_block\", Count:$(another_cool-macr0)}],NoGravity:1b,NoBasePlate:1b}"
-# print(Highlighter.nbt("{Invisible: 1b, ArmorItems:[{},{},{},{id:\"emerald_block\", Count:$(another_cool-macr0)}],NoGravity:1b,NoBasePlate:1b}"))
-# print(Highlighter.highlight("say @a[scores={kills=5,deaths=20..}]"))
-# print(Highlighter.target("@e[type=pig]"))
-print(Highlighter.highlight(a))
-# print(Highlighter.split(a))
